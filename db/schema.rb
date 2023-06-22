d194d11871f0deb71cda1905f4d935aeac84690c
Zach Weber <weberzt@gmail.com>
2023-06-22 15:03:57 -0600

    Remove accidentally added model member

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_205814) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_210057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
-    t.string "string"
     t.string "last_name"
     t.string "email"
     t.string "location"
