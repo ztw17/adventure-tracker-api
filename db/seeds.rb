b799ff4066455caabc99109b05bd2c40f77efacc
Zach Weber <weberzt@gmail.com>
2023-06-23 08:14:16 -0600

    Add activity seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 User.destroy_all
+Activity.destroy_all
 
 # Users
 User.create(first_name: "Zach", last_name: "Weber", email: "zach@email.io", location: "Denver, CO", username: "ztw", password: "password")
 User.create(first_name: "Paul", last_name: "Bramos",email: "paul@email.io", location: "Denver, CO", username: "pauljb", password: "password")
+
+# Activities
+Activity.create(name: "Mt. Lady Washington & Storm Peak", location: "Allenspark, CO", trail: ["Longs Peak Trail", "The Boulderfield"], distance: 12.05, elevation_gain: 4432, date: "2023-06-14")
+Activity.create(name: "Twin Sisters Peak", location: "Boulder, CO", trail: ["Gross Reservoir Trail", "CR 68J"], distance: 6.8, elevation_gain: 1299, date: "2023-06-19")
