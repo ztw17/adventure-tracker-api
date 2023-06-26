8925bc4b091efbe742090477e2c11b88a0bc5927
Zach Weber <weberzt@gmail.com>
2023-06-26 12:02:07 -0600

    Encrypt passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+    has_secure_password
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ User.destroy_all
 Activity.destroy_all
 
 # Users
-User.create(first_name: "Zach", last_name: "Weber", email: "zach@email.io", location: "Denver, CO", username: "ztw", password: "password")
-User.create(first_name: "Paul", last_name: "Bramos",email: "paul@email.io", location: "Denver, CO", username: "pauljb", password: "password")
+User.create(first_name: "Zach", last_name: "Weber", email: "zach@email.io", location: "Denver, CO", username: "ztw", password: BCrypt::Password.create("muirwoods"))
+User.create(first_name: "Paul", last_name: "Bramos",email: "paul@email.io", location: "Denver, CO", username: "pauljb", password: BCrypt::Password.create("olympicnational"))
 
 # Activities
 Activity.create(name: "Mt. Lady Washington & Storm Peak", location: "Allenspark, CO", trail: ["Longs Peak Trail", "The Boulderfield"], distance: 12.05, elevation_gain: 4432, date: "2023-06-14", notes: "Completely dry below 11,500', then patchy snow above. No true path to Mt. Lady Washington or Storm Peak.")
