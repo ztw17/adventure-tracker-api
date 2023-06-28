154a8d597705ee1ec7bf9a75326ba795f5d78d71
Zach Weber <weberzt@gmail.com>
2023-06-28 08:23:40 -0600

    Create Tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_140235) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_28_142303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_140235) do
     t.text "notes"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
